a9acdde65d2dcb616fa8754346b9de471b835659
Cojok <cojokka@gmail.com>
2025-06-29 18:31:20 +0200

    fix: update migration scripts and data source

--- api/src/data-source.ts
+++ api/src/data-source.ts
@@ -1,6 +1,26 @@
 import 'dotenv/config';
-import { DataSource } from 'typeorm';
-import { CustomSnakeNamingStrategy } from './common/helpers';
+import { DataSource, DefaultNamingStrategy } from 'typeorm';
+
+class CustomSnakeNamingStrategy extends DefaultNamingStrategy {
+  tableName(className: string, customName: string): string {
+    return customName ? customName : this.camelToSnakeCase(className);
+  }
+
+  columnName(
+    propertyName: string,
+    customName: string,
+    _embeddedPrefixes: string[],
+  ): string {
+    return customName ? customName : this.camelToSnakeCase(propertyName);
+  }
+
+  private camelToSnakeCase(str: string): string {
+    return str
+      .replace(/([A-Z])/g, '_$1')
+      .toLowerCase()
+      .replace(/^_/, '');
+  }
+}
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -9,7 +29,8 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: ['dist/entities/*.js'],
+  entities: ['dist/entities/**/*.js'],
   migrations: ['dist/migrations/*.js'],
   namingStrategy: new CustomSnakeNamingStrategy(),
+  logger: 'advanced-console',
 });
